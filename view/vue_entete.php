d7acd1c30de7481a79fa44f9b421622758cbe421
Joshua Thebaudin <joshua.thebaudin@stcharles-stecroix.org>
2024-04-03 09:49:43 +0200

    RM-158 <Ajout Header>

--- view/vue_entete.php
+++ view/vue_entete.php
@@ -54,7 +54,9 @@
                 <li class="nav-item">
                     <a class="nav-link text-white" aria-current="page" href="index.php#home">Accueil</a>
                 </li>
-
+                <li class="nav-item">
+                    <a class="nav-link text-white" aria-current="page" href="index.php#news">Actualités</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="index.php#principes">Le principe</a>
                 </li>
