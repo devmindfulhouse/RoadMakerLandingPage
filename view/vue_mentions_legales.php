a2f34d2523e18fb5b21ded48beadf2779ae505bf
Joshua Thebaudin <joshua.thebaudin@stcharles-stecroix.org>
2024-07-10 13:50:11 +0200

    Ajout droits de copie et de réutilisation

--- view/vue_mentions_legales.php
+++ view/vue_mentions_legales.php
@@ -13,7 +13,7 @@ include_once('view/vue_entete.php')
             </div>
             <div class="mentions--container__identity">
                 <section class="mentions--container__identity__item ">
-                    <h2>Identité de l'éditeur :</h2>
+                    <h2 class="fw-semibold">Identité de l'éditeur :</h2>
                     <address>
                         <strong>MINDFUL HOUSE</strong><br>
                         Adresse : 57 Boulevard Demorieux, 72100 Le Mans, France<br>
@@ -23,6 +23,18 @@ include_once('view/vue_entete.php')
                         Mail : <a href="mailto:contact@mindful.house.fr">contact@mindful.house.fr</a>
                     </address>
                 </section>
+            </div>
+            <div class="copy--container mx-3 mx-md-0 mb-5 mb-md-0">
+                <section class="copy--container__item">
+                    <h2 class="fw-semibold">Droit de copie et droit de réutilisation :</h2>
+                    <h3 class="fw-medium mt-3">Droit de copie :</h3>
+                    <p>Le texte de ce site web est protégé par les droits d'auteur de <strong>Mindful House</strong> sauf indication contraire. Tous droits réservés.<br>
+                        Les images et vidéos de ce site web sont la propriété de leurs créateurs respectifs et sont utilisées sous licence ou avec leur autorisation.
+                    </p>
+                    <h3 class="fw-medium mt-4">Droit de réutilisation :</h3>
+                    <p>Vous êtes autorisé à consulter et à utiliser le texte de ce site web à des fins personnelles et non commerciales uniquement.<br>
+                        Vous ne pouvez pas reproduire, distribuer, modifier ou afficher le texte sans le consentement écrit préalable de <strong>Mindful House</strong>.
+                    </p>
             </div>
         </div>
 
