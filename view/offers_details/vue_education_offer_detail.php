5e3172e481da19df6c3b08afd8aa6f9459ce2ece
Joshua Thebaudin <joshua.thebaudin@stcharles-stecroix.org>
2024-07-10 16:36:03 +0200

    ajout des USP

--- view/offers_details/vue_education_offer_detail.php
+++ view/offers_details/vue_education_offer_detail.php
@@ -6,16 +6,49 @@
 
     <div class="main-content">
         <section class="education_offer--container">
-            <h1 class="fw-bolder text-center my-3 mb-6 mb-md-5 fs-2">Notre offre pour l'education et la pédagogie d'apprentissage</h1>
+            <h1 class="fw-bolder text-center my-3 mb-5 mb-md-3 fs-2">Notre offre pour l'education et la pédagogie d'apprentissage</h1>
+
+            <div class="text-center my-5 mx-3 mx-md-0">
+                <p>
+                    RoadMaker est une solution digitale permettant de gamifier vos cours et activités dans le domaine de l’éducation.
+                    Cette plateforme intuitive et facile à utiliser offre un large éventail de fonctionnalités.
+                </p>
+                <p>
+                    Choisir RoadMaker, c'est augmenter la motivation et l'engagement des élèves, améliorer l'apprentissage et la rétention des connaissances,
+                    booster la productivité et la performance, renforcer la cohésion de groupe et le sentiment d'appartenance.
+                </p>
+                <p >
+                    Idéal pour les établissements scolaires souhaitant passer au numérique et proposer des expériences d'apprentissage plus modernes et engageantes,
+                    RoadMaker aide à motiver et fidéliser les élèves tout en améliorant les résultats en matière d'apprentissage, de performance et d'engagement.
+                </p>
+            </div>
             <div class="videos--education--container d-flex flex-column flex-lg-row gap-3 align-items-center justify-content-around my-3 my-lg-0">
-                <div class="video--education--container--item d-flex flex-column justify-content-center align-items-center">
+                <div class="video--education--container--item d-flex flex-column justify-content-center align-items-center mt-4">
                     <iframe src="https://www.youtube-nocookie.com/embed/Lq98waDaXRE?si=dDhwFp5fjbGJ8yRx"
                             title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
 
                     </iframe>
-                    <h3 class="fw-bold fs-5 text-center mx-5 mx-lg-0">Intégration à la bibliothèque universitaire du Mans grâce à RoadMaker</h3>
+                    <h3 class="fw-bold fs-5 text-center mx-5 mx-md-0 mb-5">Intégration à la bibliothèque universitaire du Mans grâce à RoadMaker</h3>
+                </div>
+            </div>
+            <div class="d-flex  flex-column my-4">
+                <strong class="mb-5 text-center">
+                    Demandez votre démo gratuite dès aujourd'hui et découvrez comment RoadMaker peut transformer vos activités !
+                </strong>
+                <div class="d-flex flex-row align-items-center justify-content-center mb-sm-5 mb-md-0 gap-3">
+                    <a href=""
+                       class="button__reservation text-white border-0 py-3 px-4 rounded-2 fs-6 fw-semibold w-25  text-center text-decoration-none mb-5 "
+                       role="button"
+                       onclick="Calendly.initPopupWidget({url: 'https://calendly.com/mindful-house-contact?primary_color=00b6f0'});return false;">
+                        Réservez une démo
+                    </a>
+                    <a href="index.php#contact"
+                       class="button__reservation text-white border-0 py-3 px-4 rounded-2 fs-6 fw-semibold w-25 text-center text-decoration-none mb-5 "
+                       role="button">
+                        Nous contacter
+                    </a>
                 </div>
             </div>
 
--- view/offers_details/vue_tourism_offer_detail.php
+++ view/offers_details/vue_tourism_offer_detail.php
@@ -6,14 +6,50 @@
 
     <div class="main-content" >
         <section class="tourism_offer--container">
-            <h1 class="fw-bolder text-center my-3 mb-6 mb-md-5 fs-2">Notre offre pour le tourisme et lieux culturels</h1>
-            <h4 class="col-lg-12 m-5 fw-bold fs-5 text-center mt-2 mx-3">Grace à Road Maker, notre partenaire EcoMobile Loisirs réalise des parcours innovants</h4>
+            <h1 class="fw-bolder text-center my-3 mb-6 mb-md-5 fs-2">Notre offre pour le tourisme et les lieux culturels</h1>
+
+            <div class="text-center my-5 mx-3 mx-md-0">
+                <p>
+                    RoadMaker est une solution digitale permettant de gamifier vos processus d’intégration, vos événements d’entreprise et vos formations.
+                    Cette plateforme intuitive et facile à utiliser offre un large éventail de fonctionnalités.
+                </p>
+                <p>
+                    Choisir RoadMaker, c’est renforcer la motivation et l’engagement de vos employés,
+                    améliorer l’efficacité des événements et des formations, favoriser la cohésion d’équipe et le sentiment d’appartenance.
+                </p>
+                <p>
+                    Idéal pour les entreprises désireuses de moderniser leurs pratiques et d'offrir des expériences d'apprentissage interactives et engageantes,
+                    RoadMaker contribue à fidéliser et à motiver les collaborateurs tout en optimisant les résultats en termes d'apprentissage,
+                    de performance et d'engagement. Demandez votre démo gratuite dès aujourd'hui et découvrez comment RoadMaker peut transformer vos initiatives d'entreprise !
+                </p>
+            </div>
+
+            <div class="d-flex  flex-column my-4">
+                <strong class="mb-5 text-center">
+                    Demandez votre démo gratuite dès aujourd'hui et découvrez comment RoadMaker peut transformer vos activités !
+                </strong>
+                <div class="d-flex flex-row align-items-center justify-content-center mb-sm-5 mb-md-0 gap-3">
+                    <a href=""
+                       class="button__reservation text-white border-0 py-3 px-4 rounded-2 fs-6 fw-semibold w-25  text-center text-decoration-none mb-5 "
+                       role="button"
+                       onclick="Calendly.initPopupWidget({url: 'https://calendly.com/mindful-house-contact?primary_color=00b6f0'});return false;">
+                        Réservez une démo
+                    </a>
+                    <a href="index.php#contact"
+                       class="button__reservation text-white border-0 py-3 px-4 rounded-2 fs-6 fw-semibold w-25 text-center text-decoration-none mb-5 "
+                       role="button">
+                        Nous contacter
+                    </a>
+                </div>
+            </div>
+
             <div class="container px-0">
                 <div class="row">
-                    <div class="col-lg-12 d-flex flex-column justify-content-center">
-                        <div class="videos--container d-flex flex-column flex-lg-row gap-3 align-items-center ">
+                    <div class="col-lg-12">
+                        <h4 class="fw-bold fs-5 text-center mt-5 mb-4 mx-3">Grâce à Road Maker, notre partenaire EcoMobile Loisirs réalise des parcours innovants</h4>
+                        <div class="videos--container d-flex flex-column flex-lg-row gap-3 align-items-center">
                             <div class="video--container--item d-flex flex-column justify-content-center align-items-center">
-                                <iframe  height="200" src="https://www.youtube-nocookie.com/embed/DE4B52U-OAk?si=YXSgBtOgGl6-9lUu"
+                                <iframe height="200" src="https://www.youtube-nocookie.com/embed/DE4B52U-OAk?si=YXSgBtOgGl6-9lUu"
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
@@ -22,12 +58,11 @@
                             </div>
                             <div class="video--container--item d-flex flex-column justify-content-center align-items-center">
                                 <iframe height="200" src="https://www.youtube-nocookie.com/embed/wubP6dVs58Y?si=McO7yOJ6prmwDcAX"
-
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                                 </iframe>
-                                <h3 class="fw-bold fs-5 text-center px-2">Retour en image des circuits proposées par EcoMobile Loisirs en 2023</h3>
+                                <h3 class="fw-bold fs-5 text-center px-2">Retour en image des circuits proposés par EcoMobile Loisirs en 2023</h3>
                             </div>
                             <div class="video--container--item d-flex flex-column justify-content-center align-items-center">
                                 <iframe height="200" src="https://www.youtube-nocookie.com/embed/7NAFA3kL2fY?si=DdFxm_gT0DPUP9cu"
@@ -39,7 +74,9 @@
                             </div>
                         </div>
                     </div>
-                    <h4 class="col-lg-12 mt-5 fw-bold fs-5 text-center mt-2 mx-3"> Pour en savoir plus sur EcoMobile Loisirs
+
+                    <div class="col-lg-12 my-5">
+                        <h4 class="fw-bold fs-5 text-center mt-2 mx-3">Pour en savoir plus sur EcoMobile Loisirs</h4>
                         <div class="socials--container d-flex flex-column flex-lg-row gap-5 align-items-center justify-content-center mb-5 mt-4">
                             <div class="social--container--item d-flex flex-column justify-content-center align-items-center mt-3">
                                 <div class="social--container--item--icon d-flex justify-content-center align-items-center">
@@ -54,7 +91,7 @@
                                     <i class="fa-brands fa-youtube"></i>
                                 </div>
                                 <h4 class="fw-bold fs-5 text-center mt-2 mx-3">
-                                    <a href="https://www.youtube.com/@ecomobileloisirs" target="_blank">Chaine Youtube</a>
+                                    <a href="https://www.youtube.com/@ecomobileloisirs" target="_blank">Chaîne Youtube</a>
                                 </h4>
                             </div>
                             <div class="social--container--item d-flex flex-column justify-content-center align-items-center mt-3">
@@ -66,11 +103,11 @@
                                 </h4>
                             </div>
                         </div>
-                    </h4>
-                </div>
+                    </div>
                 </div>
             </div>
         </section>
+
         <div class="back_to_last_page--container">
             <a href="index.php?section=index#offer" class="back_to_last_page--link d-flex justify-content-center align-items-center text-decoration-none">
                 <div class="back_to_last_page--icon d-flex justify-content-center align-items-center">
